45f087fd9e605a2c7bc8ccfb58f3132e50816a2f
Panudech031-9 <moneyak001@gmail.com>
2023-09-06 01:58:30 +0700

    commit frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [books,setBooks] = useState([])
+
+  const fetchData = () =>{
+    axios.get('https://good-gray-binturong-wig.cyclic.app/api')
+    .then((response)=>{
+      setBooks(response.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="d-flex align-items-center flex-column mt-5">
+        <h1 className='mb-2'>All Books</h1>
+          <div className="d-flex align-items-end flex-column">
+              <table className="table table-bordered mt-3 w-auto">
+                  <thead className="text-center">
+                      <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col">Name Book</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Tel</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                    {books.map((book,index)=>(
+                      <tr key={book._id} className="align-middle">
+                          <th scope="row" className="text-center">{index+1}</th>
+                          <td>{book.bookname}</td>
+                          <td>{book.author}</td>
+                          <td>{book.tel}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+              </table>
+          </div>
+      </div>
+    </>
   );
 }
 
